7ebbd1c6b9518c8d68ec3c5aa29a154942269d2b
gerardggf <gerardggf@users.noreply.github.com>
2025-08-18 21:36:24 +0000

    deploy: 0813401c992c651bf5ad7035247d71873dd5f7cf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "da1adb004e87967016257082ee123aad",
+const RESOURCES = {"flutter_bootstrap.js": "732db7c375be9036f4ece49d7df3256d",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
